e363e72bf8e788b162dd091b8448025998e50f9d
WS <ing.wsoto@gmail.com>
2021-03-27 06:50:05 -0500

    Swagger

---
+++ springboot-mongodb/src/main/java/co/edu/poli/mongodb/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package co.edu.poli.mongodb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket customerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+                .select()                 
+                .apis(RequestHandlerSelectors.basePackage("co.edu.poli.mongodb.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(mongoCustomerApiInfo());
+	}
+	
+	private ApiInfo mongoCustomerApiInfo() {
+		return new ApiInfoBuilder().
+				title("Spring Boot REST API").
+				description("Customer REST API")
+				.contact(new Contact("Web App Mongo", "", "wsoto@poligran.edu.co")).
+				version("0.0.1").
+				build();
+	}
+}
