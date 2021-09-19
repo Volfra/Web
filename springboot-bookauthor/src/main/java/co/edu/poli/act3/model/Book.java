2ee4d9075cecada337bdfd4195d2564038491b6f
Volfra <ing.wsoto@gmail.com>
2021-09-19 15:26:48 -0500

    ManytoMany OK

--- springboot-bookauthor/src/main/java/co/edu/poli/act3/model/Book.java
+++ springboot-bookauthor/src/main/java/co/edu/poli/act3/model/Book.java
@@ -3,7 +3,6 @@ package co.edu.poli.act3.model;
 import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
